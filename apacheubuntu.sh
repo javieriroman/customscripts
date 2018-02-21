7208f1abb8818dc68c7069c0e9de0222cff11ba3
javieriroman <j.roman.upr@gmail.com>
2018-02-21 16:48:31 -0600

    Update apacheubuntu.sh

--- apacheubuntu.sh
+++ apacheubuntu.sh
@@ -1,7 +1,7 @@
- #!/bin/bash
+#!/bin/bash
 
 
 ## Install AMP
-apt-get install apache2 apache-utils php php-mysql libapache2-mod-php -y
+apt-get install apache2 apache2-utils php php-mysql libapache2-mod-php -y
 
 service apache2 start
