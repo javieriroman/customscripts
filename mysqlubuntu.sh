ef88323fd134d0d5fdae2af30c6949d9bd965668
javieriroman <j.roman.upr@gmail.com>
2018-02-21 18:24:23 -0600

    Update mysqlubuntu.sh

--- mysqlubuntu.sh
+++ mysqlubuntu.sh
@@ -2,3 +2,10 @@
 export DEBIAN_FRONTEND=noninteractive
 apt-get -y install mysql-server 
 service mysql start
+apt-get -y install unzip
+#https://github.com/datacharmer/test_db
+wget https://github.com/datacharmer/test_db/archive/master.zip
+unzip master.zip
+cd test_db-master/
+mysql < employees.sql
+mysql  -t < test_employees_md5.sql
