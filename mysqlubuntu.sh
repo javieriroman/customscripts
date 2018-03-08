d96c49b0510ee097ec612d08e86577d02dcdbc86
javieriroman <j.roman.upr@gmail.com>
2018-03-08 09:30:07 -0600

    Update mysqlubuntu.sh

--- mysqlubuntu.sh
+++ mysqlubuntu.sh
@@ -12,4 +12,5 @@ mysql < employees.sql
 mysql  -t < test_employees_md5.sql
 iptables -A input -i eth0 -s 10.0.0.0/24 -p tcp --destination-port 3306 -j ACCEPT
 mysql -e "grant all privileges on employees.* to 'javier'@'%' identified by 'Diegoroman@18'"
-sed -i ‘s/127.0.0.1/10.0.0.5/’ /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i '/bind-address/c\bind-address=10.0.0.10'  /etc/mysql/mysql.conf.d/mysqld.cnf
+service mysql restart
