78680301007eababcc40aebad192d59262377ea0
javieriroman <j.roman.upr@gmail.com>
2018-03-08 12:13:38 -0600

    change to curl

--- apacheubuntu.sh
+++ apacheubuntu.sh
@@ -4,7 +4,4 @@ apt-get update
 ## Install AMP
 apt-get install apache2 apache2-utils php php-mysql php-pdo libapache2-mod-php wget -y
 service apache2 start
-wget https://raw.githubusercontent.com/javieriroman/customscripts/master/simplephp.php
-mv simplephp.php index.php
-mv index.php /var/www/html/
-service apache2 restart
+curl -0 https://raw.githubusercontent.com/javieriroman/customscripts/master/simplephp.php > /var/www/html/index.php
