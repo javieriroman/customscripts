bf13dcfded31677d9e4517acc3019b56afdcf6a3
javieriroman <j.roman.upr@gmail.com>
2018-03-08 12:28:32 -0600

    taking out packages not need it

--- apacheubuntu.sh
+++ apacheubuntu.sh
@@ -2,6 +2,6 @@
 
 apt-get update
 ## Install AMP
-apt-get install apache2 apache2-utils php php-mysql php-pdo libapache2-mod-php wget -y
-service apache2 start
+apt-get install apache2 apache2-utils php php-mysql php-pdo -y
 curl -0 https://raw.githubusercontent.com/javieriroman/customscripts/master/simplephp.php > /var/www/html/index.php
+service apache2 start
