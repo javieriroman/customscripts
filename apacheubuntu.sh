2f045d997799fbda4d423f1db6ec909183ea653f
javieriroman <j.roman.upr@gmail.com>
2018-03-08 15:41:28 -0600

    Update apacheubuntu.sh

--- apacheubuntu.sh
+++ apacheubuntu.sh
@@ -3,5 +3,6 @@
 apt-get update
 ## Install AMP
 apt-get install apache2 apache2-utils php php-mysql php-pdo -y
-curl -0 https://raw.githubusercontent.com/javieriroman/customscripts/master/simplephp.php > /var/www/html/index.php
+#curl -0 https://raw.githubusercontent.com/javieriroman/customscripts/master/simplephp.php > /var/www/html/index.php
+curl -0 https://raw.githubusercontent.com/javieriroman/customscripts/master/simplephp-v2 > /var/www/html/index.php
 service apache2 start
