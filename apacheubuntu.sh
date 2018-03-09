baa41b72891090277b16b7bbdd8b170138ec1c69
javieriroman <j.roman.upr@gmail.com>
2018-03-09 00:31:49 -0600

    Update apacheubuntu.sh

--- apacheubuntu.sh
+++ apacheubuntu.sh
@@ -9,6 +9,6 @@ slepp 15
 apt-get install php-mysql php-common -y
 sleep 15
 curl -0 https://raw.githubusercontent.com/javieriroman/customscripts/master/simplehtml > /var/www/html/index.html
-curl -0 https://raw.githubusercontent.com/javieriroman/customscripts/master/simplephp-v2 > /var/www/html/index.php
+curl -0 https://raw.githubusercontent.com/javieriroman/customscripts/master/simplephp.php > /var/www/html/index.php
 sleep 15
 service apache2 start
