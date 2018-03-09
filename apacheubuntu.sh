0da66624f31949866e171aabac44d34850938507
javieriroman <j.roman.upr@gmail.com>
2018-03-08 23:52:52 -0600

    Update apacheubuntu.sh

--- apacheubuntu.sh
+++ apacheubuntu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+export DEBIAN_FRONTEND="noninteractive"
 apt-get update
 ## Install AMP
 apt-get install apache2 -y
 apt-get install php libapache2-mod-php -y
-apt-get install php-mysql php-pdo -y
+apt-get install php-mysql php-common -y
 curl -0 https://raw.githubusercontent.com/javieriroman/customscripts/master/simplehtml > /var/www/html/index.html
 curl -0 https://raw.githubusercontent.com/javieriroman/customscripts/master/simplephp-v2 > /var/www/html/index.php
 service apache2 start
