39add932ad454d81b134c1d3e5c330ecaf974e01
javieriroman <j.roman.upr@gmail.com>
2019-05-20 21:53:05 -0500

    mysql-client missing

--- apacheubuntuv2.sh
+++ apacheubuntuv2.sh
@@ -2,15 +2,15 @@
 export DEBIAN_FRONTEND="noninteractive"
 apt-get update
 ## Install AMP
-apt-get install apache2 unzip -y
+apt-get install apache2 unzip mysql-client -y
 curl -0 https://raw.githubusercontent.com/javieriroman/customscripts/master/simplehtml > /var/www/html/index.html
 curl -0 https://raw.githubusercontent.com/javieriroman/customscripts/master/simplephpv2.php > /var/www/html/index.php
-curl -0 https://github.com/datacharmer/test_db/archive/master.zip
+wget https://github.com/datacharmer/test_db/archive/master.zip
+unzip master.zip
+cd test_db-master/
+mysql -h myjrs.mysql.database.azure.com -u javier@myjrs -pMytest2019 < employees.sql
 apt-get install php libapache2-mod-php -y
 apt-get install php-mysql php-common -y
 service apache2 start
 apt-get install --reinstall php-mysql php-common -y
 service apache2 restart
-unzip master.zip
-cd test_db-master/
-mysql -h myjrs.mysql.database.azure.com -u javier@myjrs -pMytest2019 < employees.sql
