4e2d889dc0983a8e3284f979831cee200d698981
javieriroman <j.roman.upr@gmail.com>
2019-05-20 19:17:25 -0500

    Create apacheubuntuv2.sh

---
+++ apacheubuntuv2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export DEBIAN_FRONTEND="noninteractive"
+apt-get update
+## Install AMP
+apt-get install apache2 unzip -y
+curl -0 https://raw.githubusercontent.com/javieriroman/customscripts/master/simplehtml > /var/www/html/index.html
+curl -0 https://raw.githubusercontent.com/javieriroman/customscripts/master/simplephpv2.php > /var/www/html/index.php
+curl -0 https://github.com/datacharmer/test_db/archive/master.zip
+apt-get install php libapache2-mod-php -y
+apt-get install php-mysql php-common -y
+service apache2 start
+apt-get install --reinstall php-mysql php-common -y
+service apache2 restart
+unzip master.zip
+cd test_db-master/
+mysql -h myjrs.mysql.database.azure.com -u javier@myjrs -pMytest2019 < employees.sql
